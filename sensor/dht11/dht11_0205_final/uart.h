e25376075abd1b6188337de6f40efd3fd476115b
kms19910502 <audtjr37@gmail.com>
2023-03-31 09:51:34 +0900

    test

--- sensor/dht11/dht11_0205_final/uart.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef UART_H_
-#define UART_H_
-
-void USART_Init(void);
-unsigned char USART_Receive(void);
-void USART_Transmit(unsigned char data);
-void strTransmit(char *str);
-
-#endif /* UART_H_ */
